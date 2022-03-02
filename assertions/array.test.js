b95858e679fb75bb18d97212cb1fdcfce7fbd965
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-03-02 08:36:18 +0000

    UNIT test Object [ oftypes.array_ ]

--- assertions/array.test.js
+++ assertions/array.test.js
@@ -1,11 +1,16 @@
 import { array_ } from '../index.js'
-import { output__ } from 'trythistrythat/lib/exporter.js'
-import { describe, failed, oki } from 'trythistrythat'
+import { deeeeepStrictEqual, describe, end_test, failed, oki, separator } from 'trythistrythat'
 
 export default async () => {
-    await describe( 'UNIT test for array_ type check' )
+    await describe( 'UNIT test for Object [ oftypes.array_ ]' )
+    console.log()
     
     let error
+    
+    await describe( 'variable is an array of {number[]} -> [ 5 ]' )
+    await describe( 'returns true'.yellow() )
+    console.log()
+    
     error = await oki( async () => {
         
         return {
@@ -16,15 +21,29 @@ export default async () => {
         
     } )
     
+    await separator()
+    
+    await describe( 'variable is an array of {number[]} -> [ 5 ]' )
+    await describe( 'returns [ true, [ 5 ] ]'.yellow() )
+    console.log()
+    
+    error = await deeeeepStrictEqual( async () => {
+        
+        return {
+            expected: [ true, [ 5 ] ],
+            actual: await array_( [ 5 ], undefined, true ),
+            error: 'something went wrong'
+        }
+        
+    } )
+    
     if( error instanceof Error ) {
         failed( true )
         console.log( error.message.red() )
-        output__.event.emit( 'end' )
-    }else if( error === false ){
+        end_test()
+    }else{
         
         console.log( 'test passed'.green() )
-        output__.event.emit( 'end' )
+        end_test()
     }
-    
-    
 }
