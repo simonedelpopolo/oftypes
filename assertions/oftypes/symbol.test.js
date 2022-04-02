e3039a459a8a70b21bff40fe3f43d9aecb058482
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-02 10:38:25 +0000

    v1.4.3 REFACTORED Object [ oftypes.symbol_ ]

---
+++ assertions/oftypes/symbol.test.js
@@ -0,0 +1,77 @@
+import * as tttt from 'trythistrythat'
+import { symbol_ } from '../../public.js'
+
+export default async () => {
+    let error
+
+    tttt.describe( '# UNIT tests for **Object [ oftypes.symbol_ ]**'.bg_yellow().strong().underline(), '\n' )
+    tttt.separator()
+
+    tttt.describe( '\n', '## (1) "symbol_variable" is Symbol'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "variable" set to Symbol.for("variable")' )
+    tttt.describe( ' ➡ argument "payback" set to "true"' )
+    tttt.describe( ' ➡ returns array [ true, Symbol(variable), { type: \'Symbol\' } ]'.yellow(), '\n' )
+    tttt.separator()
+
+    error = await tttt.deeeeepStrictEqual( async () => {
+
+        let symbol_variable = Symbol.for( 'variable' )
+
+        return {
+            expected: [ true, Symbol.for( 'variable' ), { type: 'Symbol' } ],
+            actual: await symbol_( symbol_variable, undefined, true ),
+            error: 'test failed'
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error, '\n' )
+    }else
+        tttt.describe( '\n', 'test passed'.green(), '\n' )
+
+    tttt.separator()
+    tttt.describe( '\n', '## (2) "null & undefined" variable'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "variable" set to "undefined" & "null"' )
+    tttt.describe( ' ➡ returns false'.yellow(), '\n' )
+    tttt.separator()
+
+    error = await tttt.oki( async () => {
+
+        let undefined_variable = undefined
+
+        return {
+            expected: false,
+            actual: await symbol_( undefined_variable ),
+            error: 'test failed'
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    error = await tttt.oki( async () => {
+
+        let undefined_variable = null
+
+        return {
+            expected: false,
+            actual: await symbol_( undefined_variable ),
+            error: 'test failed'
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    tttt.end_test( tttt.id() )
+}
