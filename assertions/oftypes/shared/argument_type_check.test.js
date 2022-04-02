f965b302c3ed35a33faacdfc7c8eacd59dd5e6ab
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-02 09:30:07 +0000

    v1.2.2 ADDED Object [ oftypes.shared.argument_type_check ] ⇩
    - async function generator to type check the arguments passed to all the Object oftype.[type_functions]
    - UNIT test argument_type_check.test.js

---
+++ assertions/oftypes/shared/argument_type_check.test.js
@@ -0,0 +1,60 @@
+import * as tttt from 'trythistrythat'
+import { argument_type_check } from '../../../index.js'
+
+export default async () => {
+
+    let error
+
+    tttt.describe( '# UNIT tests for **Object [ oftypes.shared.argument_type_check ]**'.bg_yellow().strong().underline(), '\n' )
+    tttt.separator()
+
+    tttt.describe( '\n', '## (1) this UNIT do not rejects.'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "returns" true'.yellow(), '\n' )
+    tttt.separator()
+
+    let argument_type_check_true = false
+    for await ( const type_checks of argument_type_check( true, true, { true:true, false: false } ) )
+        if( ! ( type_checks instanceof Error ) ) argument_type_check_true = type_checks
+
+    error = await tttt.oki( () => {
+
+        return {
+            expected: true,
+            actual: argument_type_check_true
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    tttt.separator()
+
+    tttt.describe( '\n', '## (2) this UNIT rejects with <oftypes.shared><OftypesError>.'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "returns" OftypesError'.yellow(), '\n' )
+    tttt.separator()
+
+    argument_type_check_true = false
+    for await ( const type_checks of argument_type_check( true, 'strict as string will throws/rejects', { true:true, false: false } ) )
+        if( type_checks instanceof Error ) argument_type_check_true = type_checks
+
+    error = await tttt.oki( () => {
+
+        return {
+            expected: 'OftypesError',
+            actual: argument_type_check_true.name
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    tttt.end_test( tttt.id() )
+}
