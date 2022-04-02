a9c484b4b65e216b33708b8d78d2c97a44d389ab
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-02 10:20:59 +0000

    v1.3.0 Object [ oftypes.number_ ]
    - ADDED functionality argument '[string=true]'
      - argument string, Default 'true' will consider 'String like Number' variable as 'Number'
      - set to 'false' will strictly type check the variable to have constructor.name === 'Number'
      - Default to 'true' to have backward compatibility with previous versions of oftypes
    
    - ADDED support to Object [ oftypes.shared.reject ] && Object [ oftypes.shared.argument_type_checking ]
      - for type check of argument 'string' @ Object [ oftypes.number_ ]

---
+++ assertions/oftypes/number.test.js
@@ -0,0 +1,86 @@
+import * as tttt from 'trythistrythat'
+import { number_ } from '../../public.js'
+
+export default async () => {
+    let error
+
+    tttt.describe( '# UNIT tests for **Object [ oftypes.number_ ]**'.bg_yellow().strong().underline(), '\n' )
+    tttt.separator()
+
+    tttt.describe( '\n', '## (1) {String}=\'11\' considered Number'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "variable" set to "11"' )
+    tttt.describe( ' ➡ argument "payback" set to "true"' )
+    tttt.describe( ' ➡ returns array [ true, \'11\', { type:\'String\' } ]'.yellow(), '\n' )
+    tttt.separator()
+
+    error = await tttt.deeeeepStrictEqual( async () => {
+
+        let variable = '11'
+
+        return {
+            expected: [ true, '11', { type:'String' } ],
+            actual: await number_( variable, undefined, true ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error, '\n' )
+    }else
+        tttt.describe( '\n', 'test passed'.green(), '\n' )
+
+    tttt.separator()
+    tttt.describe( '\n', '## (2) test for a number'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "variable" set to "10_000"' )
+    tttt.describe( ' ➡ returns false'.yellow(), '\n' )
+    tttt.separator()
+
+    error = await tttt.oki( async () => {
+
+        let variable = 10_000
+
+        return {
+            expected: true,
+            actual: await number_( variable ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    tttt.line()
+
+    tttt.separator()
+    tttt.describe( '\n', '## (3) {String}=\'10000\' considered NOT Number'.bg_yellow().strong().underline(), '\n' )
+    tttt.describe( ' ➡ "variable" set to "10000"' )
+    tttt.describe( ' ➡ argument "payback" set to "true"' )
+    tttt.describe( ' ➡ argument "string" set to "false"' )
+    tttt.describe( ' ➡ returns array [ false, \'10000\', { type:\'String\' } ]'.yellow(), '\n' )
+    tttt.separator()
+
+    error = await tttt.deeeeepStrictEqual( async () => {
+
+        let variable = '10000'
+
+
+
+        return {
+            expected: [ false, '10000', { type:'String' } ],
+            actual: await number_( variable, undefined, true, false ).catch( error => error ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( '\n', error )
+    }else
+        tttt.describe( '\n', 'test passed'.green() )
+
+    tttt.end_test( tttt.id() )
+}
