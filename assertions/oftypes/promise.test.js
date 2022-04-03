1fc916c0bdc8b2e6fc7457e99ebaac7a9b0273fa
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-03 13:01:12 +0000

    v1.6.1 REFACTORED Object [ oftypes.promise_ ] ⇩
    - UNIT test promise.test.js

---
+++ assertions/oftypes/promise.test.js
@@ -0,0 +1,59 @@
+import * as tttt from 'trythistrythat'
+import { promise_ } from '../../public.js'
+
+export default async () => {
+
+    tttt.describe( '**oftypes/promise.test.js**'.underline().strong() )
+
+    tttt.describe( '# UNIT tests for **Object [ oftypes.promise_ ]**'.bg_yellow().strong().underline() )
+    tttt.line()
+
+    let error
+
+    tttt.describe( 'variable is Promise' )
+    tttt.describe( 'returns true'.yellow() )
+    tttt.line()
+
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: true,
+            actual: await promise_( new Promise( resolve => resolve( 'ok' ) ) )
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.line()
+
+    tttt.separator()
+    tttt.line()
+
+    tttt.describe( 'variable is an AsyncFunction -> async () => {})' )
+    tttt.describe( 'returns [ false, [AsyncFunction: func], {type: \'AsyncFunction\'} ]'.yellow() )
+    tttt.line()
+
+    error = await tttt.deeeeepStrictEqual( async () => {
+
+        const func = async() => {}
+
+        return {
+            expected: [ false, func, { type: 'AsyncFunction' } ],
+            actual: await promise_( func, undefined, true ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.end_test( tttt.id() )
+}
--- public.js
+++ public.js
@@ -7,6 +7,7 @@ import {
     compare__,
     function__,
     number__,
+    promise__,
     resolvers__,
     symbol__,
     undefined__,
@@ -242,6 +243,32 @@ export function function_( variable, resolvers = { true: true, false: false }, p
  */
 export function number_( variable, resolvers = { true: true, false: false }, payback = false, string= true ) {
     return number__( variable, resolvers, payback, string )
+}
+
+/**
+ * Object [ oftypes.promise_ ]
+ *
+ * **type check for promise**.
+ *
+ * @param {any} variable - to check for
+ * @param {{true:any,false:any}} [resolvers={true:true,false:false}] - dynamically set "kind of" if/else statement to resolve the type checking.
+ * @param {boolean} [payback=false] - If true it will send back the variable value.
+ * @example
+ *     import * as oftypes from 'oftypes'
+ *
+ *     let variable = new Promise( resolve => resolve( 'ok' ) )
+ *
+ *     console.log( await oftypes.promise_( variable, {
+ *       true:'the variable is Promise',
+ *       false:'variable is NOT Promise'
+ *     }))
+ *
+ *     // **prints -> the variable is Promise**
+ * @throws {OftypesError}
+ * @returns {Promise<boolean|any|[any,any,{type:string}]> | boolean|any|[any,any,{type:string}]}
+ */
+export function promise_( variable, resolvers = { true: true, false: false }, payback = false ) {
+    return promise__( variable, resolvers, payback )
 }
 
 /**
