4e0511c618ec324f38b25265b8148fbacfa83eb4
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-08 08:11:44 +0000

    v1.7.6 REDUCED duplicate code

--- tests/oftypes/buffer.test.js
+++ tests/oftypes/buffer.test.js
@@ -1,6 +1,12 @@
 import * as tttt from 'trythistrythat'
 import { buffer_ } from '../../public.js'
 
+/**
+ * UNIT-test boolean.test.js
+ *
+ * @param {string} id - UNIT-test
+ * @returns {Promise<void> | void}
+ */
 export default async ( id ) => {
 
     tttt.describe( '# UNIT tests for **Object [ oftypes.buffer_ ]**'.bg_yellow().strong().underline() )
