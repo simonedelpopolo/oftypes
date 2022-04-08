3367e16ab6ea88348b84cde46aeb3629f574203d
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-08 08:05:54 +0000

    v1.7.3 REDUCED duplicate code

--- tests/oftypes/async.test.js
+++ tests/oftypes/async.test.js
@@ -1,6 +1,12 @@
 import * as tttt from 'trythistrythat'
 import { async_ } from '../../public.js'
 
+/**
+ * UNIT-test async.test.js
+ *
+ * @param {string} id - UNIT-test
+ * @returns {Promise<void> | void}
+ */
 export default async ( id ) => {
 
     tttt.describe( '# UNIT tests for **Object [ oftypes.async_ ]**'.bg_yellow().strong().underline() )
