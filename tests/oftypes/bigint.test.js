0fec8cc7ac191df83c1e6cebfb1d8ee575458712
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-08 08:08:35 +0000

    v1.7.4 REDUCED duplicate code

--- tests/oftypes/bigint.test.js
+++ tests/oftypes/bigint.test.js
@@ -1,6 +1,12 @@
 import * as tttt from 'trythistrythat'
 import { bigint_ } from '../../public.js'
 
+/**
+ * UNIT-test bigint.test.js
+ *
+ * @param {string} id - UNIT-test
+ * @returns {Promise<void> | void}
+ */
 export default async ( id ) => {
 
     tttt.describe( '# UNIT tests for **Object [ oftypes.bigint_ ]**'.bg_yellow().strong().underline() )
