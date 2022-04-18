0d1552704ae81d159190a7bfed956059b653a9bd
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-04-18 10:46:02 +0000

    v1.7.13 tuning up oftype_

--- tests/oftypes/oftype.test.js
+++ tests/oftypes/oftype.test.js
@@ -40,13 +40,108 @@ export default async ( id ) => {
 
     tttt.describe( 'variable is an object -> { object: null }' )
     tttt.describe( 'returns [ \'object type\', { object: null }, {type: \'Object\'} ]'.yellow() )
-    await tttt.line()
 
     error = await tttt.deeeeepStrictEqual( async () => {
 
         return {
             expected: [ 'object type', { object: null }, { type: 'Object' } ],
             actual: await oftype_( { object: null }, { Object: 'object type' }, true ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'variable is null returns null' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: 'null',
+            actual: await oftype_( null ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'variable is undefined returns undefined' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: 'undefined',
+            actual: await oftype_( undefined ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'variable is false returns Boolean' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: 'Boolean',
+            actual: await oftype_( false ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'String.length === 0 returns String' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: 'String',
+            actual: await oftype_( '' ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'resolver looks for Array get alright' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: 'alright',
+            actual: await oftype_( [ '' ], { Array: 'alright' } ),
+        }
+
+    } )
+
+    if( error instanceof Error ) {
+        tttt.failed( true )
+        tttt.describe( error )
+    }else
+        tttt.describe( 'test passed'.green() )
+
+    tttt.describe( 'resolver looks for Array get "undefined"' )
+    error = await tttt.oki( async () => {
+
+        return {
+            expected: undefined,
+            actual: await oftype_( String( '' ), { Array: 'alright' } ),
         }
 
     } )
